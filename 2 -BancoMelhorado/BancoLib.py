b712b9d1bd025ca0ce3c7f8c09e1951c48f282ef
Emerson Batista <84859510+emersongg21@users.noreply.github.com>
2023-02-05 00:40:37 +0000

    Initial commit

---
+++ 2 -BancoMelhorado/BancoLib.py
@@ -0,0 +1,78 @@
+import random
+import pickle
+
+
+class Conta():
+    def __init__(self, numConta):
+        self.numero = numConta
+        self.saldo = 0
+
+    def deposite(self, valor):
+        self.saldo = self.saldo + valor
+
+    def sacar(self, valor):
+        if self.saldo >= valor:
+            self.saldo = self.saldo - valor
+            return True
+        else:
+            return False
+
+
+class Poupanca(Conta):
+
+    def render(self):
+        self.saldo = self.saldo + self.saldo*0.01
+
+
+class Banco():
+    def __init__(self, nome):
+        self.nome = nome
+        self.contas = []
+
+    def getNome(self):
+        return self.nome
+
+    def criarConta(self):
+        num = random.randint(0, 1000)
+        c = Conta(num)
+        self.contas.append(c)
+        return num
+
+    def criarPoupanca(self):
+        num = random.randint(0, 1000)
+        p = Poupanca(num)
+        self.contas.append(p)
+        return num
+
+    def consultaSaldo(self, numConta):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                return conta.saldo
+        return -1
+
+    def depositar(self, numConta, valor):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                conta.deposite(valor)
+
+    def sacar(self, numConta, valor):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                return conta.sacar(valor)
+
+    def renderPoupanca(self, numConta):
+        for i in self.contas:
+            if i.numero == numConta and isinstance(i, Poupanca):
+                i.render()
+                return True
+        return False
+
+    def salveArquivo(self):
+        with open('conta.dados', 'wb') as arquivo:
+            # Serializa o objeto e salva no arquivo
+            pickle.dump(self.contas, arquivo)
+
+    def carregarArquivo(self):
+        with open('conta.dados', 'rb') as arquivo:
+            # Carrega o objeto serializado do arquivo
+            self.contas = pickle.load(arquivo)
---
+++ 2 -BancoMelhorado/README.md
@@ -0,0 +1,15 @@
+# Aula1
+
+## Tarefa 1 
+
+- Alterar o código para que TODO tipo de conta (Conta Corrente, Poupança, etc.) deveram descontar 0,1% do que foi depositado.
+
+## Tarefa 2 
+
+- Alterar o código para incluir uma nova classe chamada ContaBonificada
+  
+  * Depositar acumula um bônus equivalente a 0,01% do valor depositado
+
+  * Método renderBonus que CREDITA no saldo a quantidade que houver no BÔNUS, bem como zera a quantidade de bônus acumulado.
+
+  * Somente a classe ContaBonificada pode acumula o bônus
